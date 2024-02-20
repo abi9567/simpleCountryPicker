83fcda0a36915b886909b093b1e1a275d4417c3a
Abi <abijith@pixbitsolutions.com>
2024-02-20 15:54:42 +0530

    change version to 1.0.2

--- simple-country-picker/build.gradle.kts
+++ simple-country-picker/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.abi.simplecountrypicker"
     compileSdk = 34
-    version = "1.0.0"
+    version = "1.0.2"
 
     defaultConfig {
         minSdk = 24
@@ -51,7 +51,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.abi9567"
             artifactId = "simpleCountryPicker"
-            version = "1.0.0"
+            version = "1.0.2"
 //            artifact("${layout.buildDirectory}/outputs/aar/${artifactId}-release.aar")
 
             afterEvaluate {
